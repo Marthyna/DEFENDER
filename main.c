0a9da02db5131267edadfb3fd4ce615891163bdd
Marthyna <44986993+Marthyna@users.noreply.github.com>
2019-11-17 01:07:43 -0300

    Atualizações
    Inclui a abertura do arquivo texto do mapa, leitura do mesmo, geração da tela a partir da coluna 0 e impressão da mesma;
    Modificação no laço de while para do-while usando como parâmetro de repetição a flag de repetição, não mais a flag de saída.

--- main.c
+++ main.c
@@ -1,31 +1,52 @@
 #include "main.h"
 #include "menu_inicial.h"
+#include "le_mapa.h"
+#include "gera_tela.h"
+#include "imprime_tela.h"
 
 int main()
 {
     setlocale(LC_ALL, "Portuguese_Brazil");
+
+    FILE *arq;
+
     char opcao;
-    int flag_opcao_inexistente = 0;
-    int flag_sair = 0;
-    while(!flag_sair)
+    char mapa[LINHAS][COLUNAS];
+    char tela[LINHAS][COLUNAS_TELA];
+
+    int flag_repetir_menu = 0;
+    int coluna_atual = 0;
+
+    arq = fopen(FILE_MAPA, "r");
+    if(!arq)
+    {
+        printf("Erro ao abrir o arquivo do mapa.");
+        return 0;
+    }
+
+    do
     {
         opcao = menu_inicial();
         switch(opcao)
         {
             case '1':
-                printf("Você escolheu a opção 'Novo Jogo'.");
+                printf("Você escolheu a opção 'Novo Jogo'.\n");
+                le_mapa(arq, mapa);
+                gera_tela(mapa, tela, coluna_atual);
+                imprime_tela(tela);
                 break;
             case '2':
                 printf("Você escolheu a opção 'Carregar Jogo'.");
                 break;
             case '3':
                 printf("Você escolheu a opção 'Sair'.");
-                flag_sair = 1;
                 break;
             default:
                 printf("Opção inválida.");
+                flag_repetir_menu = 1;
+                break;
         }
-    }
+    } while (flag_repetir_menu);
 
     return 0;
 }
