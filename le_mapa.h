723d8ba14d0cab684621f26ac2188058d6cba844
Marthyna <44986993+Marthyna@users.noreply.github.com>
2019-11-17 16:47:00 -0300

    Atualizado
    Agora a função le_mapa() retorna uma COORDENADA, não é mais void.

--- le_mapa.h
+++ le_mapa.h
@@ -2,5 +2,5 @@
     #define LE_MAPA_H
     #include "main.h"
 
-    void le_mapa(FILE *arq, char mapa[LINHAS][COLUNAS]);
+    COORDENADA le_mapa(FILE *arq, char mapa[LINHAS][COLUNAS]);
 #endif // LE_MAPA_H
