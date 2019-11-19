0fbc24730477d3807132a7f5de405e9ec150e6a9
hiquedoscompiuter <55196941+hiquedoscompiuter@users.noreply.github.com>
2019-11-19 16:54:59 -0300

    olhar comentario!
    faltou mudar a main só mas eu n quis mexer pq n peguei o que tu comitou agora ainda

---
+++ imprime_inimigo.c
@@ -0,0 +1,18 @@
+#include "imprime_inimigo.h"
+
+void imprime_inimigo(char mapa[LINHAS][COLUNAS], COORDENADA inimigo_posicao)
+{
+    int i, j, x, y;
+    i = inimigo_posicao.linha;
+    j = inimigo_posicao.coluna;
+
+    for(x = 0; x < LINHAS; x++)
+        for(y = 0; y < COLUNAS; y++)
+            if((x != i || y != j) && (mapa[x][y] == 'X'))
+                mapa[x][y] = ' ';
+
+    mapa[i][j] = 'X';
+    mapa[i-1][j] = 'X';
+    mapa[i-1][j-1] = 'X';
+    mapa[i][j-1] = 'X';
+}
---
+++ imprime_inimigo.h
@@ -0,0 +1,5 @@
+#ifndef IMPRIME_INIMIGO_H
+    #define IMPRIME_INIMIGO_h
+    #include "main.h"
+    void imprime_inimigo(char mapa[LINHAS][COLUNAS], COORDENADA inimigo_posicao);
+#endif // IMPRIME_INIMIGO_H
