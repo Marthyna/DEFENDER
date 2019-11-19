a3a90abe3ea1f4ab6af3d881ef898cf74aa99182
hiquedoscompiuter <55196941+hiquedoscompiuter@users.noreply.github.com>
2019-11-19 16:55:31 -0300

    le mapa update

--- le_mapa.c
+++ le_mapa.c
@@ -1,12 +1,20 @@
 #include "le_mapa.h"
 
-COORDENADA le_mapa(FILE *arq, char mapa[LINHAS][COLUNAS])
+COORDENADA le_mapa(FILE *arq, char mapa[LINHAS][COLUNAS], COORDENADA naves[21])
 {
-    int i, j;
-    char c;
+    int i, j, x, y, a;
+    char c, z;
     COORDENADA nave_pos_inicial;
+    COORDENADA inimigo_pos_inicial;
 
-    for(i = 0; i < LINHAS; i++)
+    for(a = 0; a < 21; a++) // inicialização do array de coordenadas
+    {
+        naves[a].linha = 0;
+        naves[a].coluna = 0;
+    }
+
+
+    for(i = 0; i < LINHAS; i++) //varredura do mapa para encontrar a nave do jogador
     {
         for(j = 0; j < COLUNAS; j++)
         {
@@ -17,8 +25,28 @@ COORDENADA le_mapa(FILE *arq, char mapa[LINHAS][COLUNAS])
             {
                 nave_pos_inicial.linha = i;
                 nave_pos_inicial.coluna = j;
+                naves[0] = nave_pos_inicial;
+
             }
         }
     }
-    return nave_pos_inicial;
+    for (x = 0; x < LINHAS; x++)// varredura do mapa para encontrar a nave inimiga
+    {
+        for(y = 0, a = 1; y < COLUNAS; y++, a++)
+        {
+            z = fgetc(arq);
+            mapa[x][y] = z;
+            //completa a nave inimiga
+            if(z == 'X')
+            {
+                inimigo_pos_inicial.linha = x;
+                inimigo_pos_inicial.coluna = y;
+                while (a < 21)
+                {
+                    naves[a] = inimigo_pos_inicial;
+                }
+            }
+        }
+    }
+    return naves[21];
 }
--- le_mapa.h
+++ le_mapa.h
@@ -2,5 +2,5 @@
     #define LE_MAPA_H
     #include "main.h"
 
-    COORDENADA le_mapa(FILE *arq, char mapa[LINHAS][COLUNAS]);
+    COORDENADA le_mapa(FILE *arq, char mapa[LINHAS][COLUNAS], COORDENADA naves[21]);
 #endif // LE_MAPA_H
